5781e2ee05690e936c783f88af5ce6271684117a
Sakib Rahman <seu.sakib@gmail.com>
2023-09-23 16:30:03 +0600

    project init

---
+++ src/test/java/com/flightpro/flightpro/FlightProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightpro.flightpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
